ded720dfbac8b5a72f13b7d20c9350954f4eaefb
komorowskidev <komorowskidev@gmail.com>
2020-05-05 21:36:10 +0200

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/solutions-frontend'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/solutions-frontend/'}),
+);
