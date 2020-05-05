c6c79a0f1fce6a8f1df4f796914b3b8738522781
komorowskidev <komorowskidev@gmail.com>
2020-05-05 21:58:34 +0200

    setting port8080 for heroku

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ app.use(express.static('./dist/solutions-frontend'));
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/solutions-frontend/'}),
 );
+
+app.listen(process.env.PORT || 8080);
